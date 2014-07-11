ef4a8324f3979d6551906c7b8c006bda47cf6bf1
Ryan <hiphubryan@gmail.com>
2014-07-11 21:47:27 +0800

    modify to rubyish

--- black_jack_game.rb
+++ black_jack_game.rb
@@ -4,9 +4,10 @@ require "pry"
 def initialize_poker
 	decks_of_poker = []
 
-	for index in 0..3 do 
-		decks_of_poker[index] = {:spades=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :hearts=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :diamonds=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :clubs=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}}
+	4.times do
+  decks_of_poker << {:spades=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :hearts=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :diamonds=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}, :clubs=>{'A'=>11, '2'=>2, '3'=>3, '4'=>4, '5'=>5, '6'=>6, '7'=>7, '8'=>8, '9'=>9, '10'=>10, 'J'=>10, 'Q'=>10, 'K'=>10}}
 	end
+
 	return decks_of_poker
 end
 
@@ -20,8 +21,8 @@ end
 def deal_card(decks_of_poker, someone)
 
 		poker = random_deck_of_poker(decks_of_poker)
-		randam_suit = poker.keys.sample
-		random_card = poker[randam_suit].keys.sample
+		random_suit = poker.keys.sample
+		random_card = poker[random_suit].keys.sample
 
 		hash = {:suit=>randam_suit, :card=>random_card, :value=>poker[randam_suit][random_card]}		
 		someone[:cards].push(hash)
